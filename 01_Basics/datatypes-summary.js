2ad37c98c0587f6507e79b38140dc1e62a81cc1c
Debkumar Mallick <141516604+Debkumar1234@users.noreply.github.com>
2025-08-12 06:10:48 +0000

    Differentiate between primitive and non primitive data types

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 3456543576654356754n
 
@@ -30,7 +30,7 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
@@ -47,3 +47,50 @@ A single variable can be assigned a number, then later a string, and then an obj
 In contrast, statically typed languages (like Java or C++) require you to explicitly declare the type of a variable, and type checks are performed at compile time, catching potential type-related errors before the program runs.
 
 */
+
+/* Stack (Primitive), Heap (Non-Primitive) */
+
+let myName = "Debkumar"
+let anotherName = myName
+
+anotherName = "Debkumar Mallick"
+
+console.log(myName) // --> Debkumar
+console.log(anotherName) // --> Debkumar Mallick
+
+/*
+Notes:
+
+    --> Primitive types → String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+    --> These are stored directly in stack memory.
+
+    --> When you assign anotherName = myName, a copy of the value is made — they become independent.
+
+    --> Changing anotherName does not affect myName, because each variable holds its own copy of the value.
+*/
+
+let obj1 = {
+    email: "debkumar@gmail.com",
+    password: "1234"
+}
+
+let obj2 = obj1
+
+obj2.email = "debkumar@google.com"
+
+console.log(obj1.email) // --> debkumar@google.com
+console.log(obj2.email) // --> debkumar@google.com
+
+/*
+Notes:
+
+    --> Non-primitive types → Object, Array, Function, etc.
+
+    --> The actual object is stored in heap memory, while the variable in the stack holds a reference (pointer) to that object.
+
+    --> When you do obj2 = obj1, both variables now point to the same object in heap.
+
+    --> Changing obj2.email changes the single shared object, so both obj1 and obj2 show the update.
+*/
+
